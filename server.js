112033c96dad068005b7e9416b7bee7725358942
rosie <rosie.bh.lee@gmail.com>
2023-07-04 13:26:18 -0700

    update app get respond send

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER,
@@ -27,7 +30,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('it is working') })
 
 app.post('/signin', (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
