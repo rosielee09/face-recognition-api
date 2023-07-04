17270f02e8f9d6415e87c8854cb809d5a8d979b6
rosie <rosie.bh.lee@gmail.com>
2023-07-04 16:47:04 -0700

    add proxy server

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('it is working');
 });
