bdc2c1cc0167e9337da9f38197a1c31dffca03ee
rosie <rosie.bh.lee@gmail.com>
2023-07-04 09:47:42 -0700

    changed server.js into controllers

---
+++ controllers/image.js
@@ -0,0 +1,64 @@
+
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = '43bd924a8ff84b85be7cc8fedc2ad097';
+  const USER_ID = 'developingwithrosie';
+  const APP_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+  return requestOptions;
+};
+
+const handleApiCall = (req, res) => {
+  fetch(
+    `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+    returnClarifaiRequestOptions(req.body.input)
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall,  
+};
--- server.js
+++ server.js
@@ -3,128 +3,53 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password: 'test5316',
+    database: 'face-recognition',
+  },
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: '987',
-      has: '',
-      email: 'john@gmail.com',
-    },
-  ],
-};
-
-// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-//   // Store hash in your password DB.
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-//   // result == true
-// });
-// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-//   // result == false
-// });
-
-app.get('/', (req, res) => {
-  res.send(database.users);
-});
+app.get('/', (req, res)=> { res.send(db.users) })
 
 app.post('/signin', (req, res) => {
-  // Load hash from your password DB.
-  bcrypt.compare(
-    'apples',
-    '$2b$10$4YeIk7gcIvV/SU4l9G4Fae/cGVCdEc4njMnvyBdj0KSGQhgXgvwRy',
-    function (err, result) {
-      console.log('first guess', res);
-    }
-  );
-  bcrypt.compare(
-    'veggies',
-    '$2b$10$4YeIk7gcIvV/SU4l9G4Fae/cGVCdEc4njMnvyBdj0KSGQhgXgvwRy',
-    function (err, result) {
-      console.log('second guess', res);
-    }
-  );
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  register.handleRegister(req, res, db, bcrypt, saltRounds);
 });
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  profile.handleProfileGet(req, res, db);
 });
 
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  image.handleImage(req, res, db);
 });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
 });
 
-// res = this is working
-// signin -->POST  = success/fail
-// register --> POST = user
-// profile/:userId --> GET = user
-// image --> PUT --> user
+const PORT = process.env.PORT
+app.listen(PORT || 3000, () => {
+  console.log(`app is running on port ${PORT}`);
+});
+
