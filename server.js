07dc1c49057d891f47b444228acb2a3b3a4db0c3
rosie <rosie.bh.lee@gmail.com>
2023-07-04 15:33:33 -0700

    add cors middleware

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ const db = knex({
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+});
 
 app.get('/', (req, res) => {
   res.send('it is working');
