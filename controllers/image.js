466b76e0ccfbece4ba4f7548ee275953c4f74788
rosie <rosie.bh.lee@gmail.com>
2023-07-05 11:15:17 -0700

    trying api key instead of variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,8 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const returnClarifaiRequestOptions = (imageUrl) => {
   // Your PAT (Personal Access Token) can be found in the portal under Authentification
-  const PAT = process.env.API_CLARIFAI;
+  const PAT = '43bd924a8ff84b85be7cc8fedc2ad097';
   const USER_ID = 'developingwithrosie';
   const APP_ID = 'face-detection';
   const IMAGE_URL = imageUrl;
