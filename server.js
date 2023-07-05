2aaa64f303342d0679d95aab14b2698d5ba6511b
rosie <rosie.bh.lee@gmail.com>
2023-07-04 17:29:20 -0700

    install node fetch

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const saltRounds = 10;
 const cors = require('cors');
 const knex = require('knex');
 
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
