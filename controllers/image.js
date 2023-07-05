27b6cf97bc2cab077f3bacd70c4c09c72a3b7ed1
rosie <rosie.bh.lee@gmail.com>
2023-07-05 14:36:58 -0700

    fetch to axios

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,47 @@
-const returnClarifaiRequestOptions = (imageUrl) => {
+const axios = require('axios');
+
+// const returnClarifaiRequestOptions = (imageUrl) => {
+//   // Your PAT (Personal Access Token) can be found in the portal under Authentification
+//   const PAT = process.env.API_CLARIFAI;
+//   const USER_ID = 'developingwithrosie';
+//   const APP_ID = 'face-detection';
+//   const IMAGE_URL = imageUrl;
+
+//   const raw = JSON.stringify({
+//     user_app_id: {
+//       user_id: USER_ID,
+//       app_id: APP_ID,
+//     },
+//     inputs: [
+//       {
+//         data: {
+//           image: {
+//             url: IMAGE_URL,
+//           },
+//         },
+//       },
+//     ],
+//   });
+
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       Authorization: 'Key ' + PAT,
+//     },
+//     body: raw,
+//   };
+//   return requestOptions;
+// };
+
+const handleApiCall = (req, res) => {
   // Your PAT (Personal Access Token) can be found in the portal under Authentification
   const PAT = process.env.API_CLARIFAI;
-  const USER_ID = 'developingwithrosie';
-  const APP_ID = 'face-detection';
-  const IMAGE_URL = imageUrl;
-
-  const raw = JSON.stringify({
+  const IMAGE_URL = req.body.id;
+  const raw = {
     user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
+      user_id: 'developingwithrosie',
+      app_id: 'detection',
     },
     inputs: [
       {
@@ -19,30 +52,51 @@ const returnClarifaiRequestOptions = (imageUrl) => {
         },
       },
     ],
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'Key ' + PAT,
-    },
-    body: raw,
   };
-  return requestOptions;
+
+  // const raw = JSON.stringify({
+  //   user_app_id: {
+  //     user_id: USER_ID,
+  //     app_id: APP_ID,
+  //   },
+  //   inputs: [
+  //     {
+  //       data: {
+  //         image: {
+  //           url: IMAGE_URL,
+  //         },
+  //       },
+  //     },
+  //   ],
+  // });
+
+  axios
+    .post(
+      'https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs',
+      raw,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Key ' + PAT,
+        },
+      }
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => res.status(400).json('unable to work with api'));
 };
 
-const handleApiCall = (req, res) => {
-  fetch(
-    `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-    returnClarifaiRequestOptions(req.body.input)
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.status(400).json('unable to work with API'));
-};
+// fetch(
+//   `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+//   returnClarifaiRequestOptions(req.body.input)
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     res.json(data);
+//   })
+//   .catch((err) => res.status(400).json('unable to work with API'));
+// };
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
